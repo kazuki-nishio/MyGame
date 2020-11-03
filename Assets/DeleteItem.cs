bfb92da91ac17282049dbdb13ed578c1b8e1bb34
kazuki-nishio <71393106+kazuki-nishio@users.noreply.github.com>
2020-11-03 10:00:06 +0900

    アイテムを動的に削除するようにした

---
+++ Assets/DeleteItem.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class DeleteItem : MonoBehaviour
+{
+    //メインカメラを入れる
+    private GameObject mainCam;
+    // Start is called before the first frame update
+    void Start()
+    {
+        mainCam = Camera.main.gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       if(this.transform.position.z<mainCam.transform.position.z)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
