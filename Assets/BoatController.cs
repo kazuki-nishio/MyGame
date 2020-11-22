f6a8be32f1e34cf1cfa2ac4ef7ab028e3911af8a
kazuki-nishio <71393106+kazuki-nishio@users.noreply.github.com>
2020-11-22 12:58:37 +0900

    FishingBoatの挙動を変更した

--- Assets/BoatController.cs
+++ Assets/BoatController.cs
@@ -11,6 +11,8 @@ public class BoatController : MonoBehaviour
     private float fishingBoatVelocityZ;
     //fishingBoatの横方向の速度
     private float fishingBoatVelocityX;
+    //fishingBoat生成時のボートの座標
+    private float boatGeneratingPoint;
     //Sin関数へ入れる角度
     private double radian = 0d;
     //プレーヤーオブジェクト
@@ -18,7 +20,7 @@ public class BoatController : MonoBehaviour
     //プレーヤーのRigidBody
     private Rigidbody sardineRigid;
 
-    // Start is called before the first frame update
+    // Start is called before the first fraboatGeneratingPointme update
     void Start()
     {
         //InstantiateObstacleを1秒ごとに呼び出す
@@ -29,6 +31,8 @@ public class BoatController : MonoBehaviour
         this.sardine = GameObject.Find("Sardine");
         //プレーヤーのRigidBodyを取得
         sardineRigid = sardine.GetComponent<Rigidbody>();
+        //ボート生成時の座標をboatGeneratingPointへ設定
+        boatGeneratingPoint = this.transform.position.z;
     }
 
     // Update is called once per frame
@@ -47,6 +51,14 @@ public class BoatController : MonoBehaviour
         this.radian += 1.2d;
         //横方向の移動速度を設定
         this.fishingBoatVelocityX = (float)System.Math.Sin(radian * (System.Math.PI / 180)) * 15f;
+        //一定距離を進んだら加速してオブジェクトを消去
+        if (boatGeneratingPoint + 500f < this.transform.position.z)
+        {
+            CancelInvoke("InstantiateObstacle");
+            fishingBoatVelocityX = 0;
+            this.fishingBoatVelocityZ = sardineRigid.velocity.z * 2.5f;
+            Invoke("boatDelete",1.5f);
+        }
         //fishingBoatの速度を設定
         this.boatRigid.velocity = new Vector3(fishingBoatVelocityX, 0, fishingBoatVelocityZ);
     }
@@ -59,6 +71,10 @@ public class BoatController : MonoBehaviour
             GameObject trashBag = Instantiate(obstaclePrefab);
             trashBag.transform.position = this.transform.position;
         }
-            
+    }
+    //ボートを消去
+    private void boatDelete()
+    {
+        Destroy(gameObject);
     }
 }
