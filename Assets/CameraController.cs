919300f4cf0bcd1f61a428dcd58642a348727715
kazuki-nishio <71393106+kazuki-nishio@users.noreply.github.com>
2020-12-12 13:47:37 +0900

    画面サイズの設定を変更した

--- Assets/CameraController.cs
+++ Assets/CameraController.cs
@@ -3,22 +3,17 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    //Sardineオブジェクトを入れる
-    private GameObject Sardine;
-    //Goalオブジェクトを入れる
-    private GameObject Goal;
-    //Sardineとカメラの位置の差
-    private float difference;
+    
+    private GameObject Sardine;//Sardineオブジェクトを入れる    
+    private GameObject Goal;//Goalオブジェクトを入れる    
+    private float difference;//Sardineとカメラの位置の差
 
     // Start is called before the first frame update
     void Start()
-    {
-        //Sardineオブジェクトを取得
-        this.Sardine = GameObject.Find("Sardine");
-        //Goalオブジェクトを取得
-        this.Goal = GameObject.Find("Goal");
-        //カメラとSardineの距離の差を求める
-        this.difference = Sardine.transform.position.z - this.transform.position.z;
+    {        
+        this.Sardine = GameObject.Find("Sardine");//Sardineオブジェクトを取得        
+        this.Goal = GameObject.Find("Goal");//Goalオブジェクトを取得      
+        this.difference = Sardine.transform.position.z - this.transform.position.z; //カメラとSardineの距離の差を求める
     }
 
     // Update is called once per frame
@@ -27,7 +22,7 @@ public class CameraController : MonoBehaviour
         if(Sardine.transform.position.z < Goal.transform.position.z)
         {
             //カメラの位置をSardineの後ろに固定
-            this.transform.position = new Vector3(0, this.transform.position.y, Sardine.transform.position.z - difference);
+            this.transform.position = new Vector3(Sardine.transform.position.x, this.transform.position.y, Sardine.transform.position.z - difference);
         }
        else
         {
